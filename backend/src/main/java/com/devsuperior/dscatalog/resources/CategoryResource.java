e8a73887edbad4b10537467be680aa544259d0c9
Vanderson Tavares <vandersontavares32@gmail.com>
2021-11-18 11:39:42 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,25 +1,34 @@
 package com.devsuperior.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
+	@Autowired
+	private CategoryService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
 		
+		List<Category> list = service.findAll();
+		
+		/*
 		list.add(new Category(1L, "Book"));
 		list.add(new Category(2L, "Eletronics"));
+		list.add(new Category(3L, "Food"));
+		*/
+		
 		return ResponseEntity.ok().body(list); //retorna a lista no corpo da resposta http
 		
 	}
